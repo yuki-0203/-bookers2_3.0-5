8db4041f1a4f0fee58ec00abd095e46f9faf5106
EC2 Default User <ec2-user@ip-172-31-23-132.ec2.internal>
2021-12-23 05:38:19 +0000

    complete

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,13 @@
 class SearchController < ApplicationController
+ 
   def search
     @model = params["search"]["model"]
     @value = params["search"]["value"]
     @how = params["search"]["how"]
     @datas = search_for(@how,@model,@value)
   end
+  
+  
   private
   
   def match(model, value) 
@@ -12,8 +15,26 @@ class SearchController < ApplicationController
       User.where(name:value)
     elsif model == 'book'
       Book.where(title:value)
+   end
+  end
+    
+  def forward(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "#{value}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "#{value}%")
+   end
+  end
+  
+
+  def backward(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{value}")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "%#{value}")
     end
   end
+
   
   def partical(model, value)
     if model == 'user'
@@ -27,11 +48,12 @@ class SearchController < ApplicationController
     case how
       when  'match'
         match(model,value)
+      when 'forward'     
+      forward(model, value)
+      when 'backward'
+      backward(model, value)
       when  'partical'
-        partical(model,value)
+      partical(model,value)
     end
   end
-
 end
-
-
