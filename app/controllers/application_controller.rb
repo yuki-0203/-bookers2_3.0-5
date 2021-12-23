cc93124f13a3858e35fe8c611252b0baa92bd43b
EC2 Default User <ec2-user@ip-172-31-23-132.ec2.internal>
2021-12-23 05:08:16 +0000

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+  
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
   get "users/show" => "users#show"
   get '/users' => "users#index"
   get "users/:id" => "users#show", as: :mypage
+  get 'search' => "search#search"
   resources :users,only: [:show,:index,:edit,:update] do
     resources :relationships, only: [:create, :destroy]
     get :followings, on: :member
